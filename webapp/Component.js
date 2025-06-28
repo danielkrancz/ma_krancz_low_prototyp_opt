b6e23751f24e0507dfc04ae5b99e28cd02fcae0d
Daniel Krancz <daniel.krancz@clouddna.at>
2025-06-28 18:41:27 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("at.hb.makrancz.lowcodeappopt.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
