d83ae9b0afbe9410d36ba9c567fa5d93459aff92
Daniel Krancz <daniel.krancz@clouddna.at>
2025-07-22 11:31:11 +0000

    Deployment

---
+++ dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"],function(e){"use strict";return e.extend("at.hb.makrancz.lowcodeappopt.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
